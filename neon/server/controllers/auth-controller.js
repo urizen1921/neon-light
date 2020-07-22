34490bdf6375bf6798a8737027d459619e4189b5
Paulo Paula <paulo.almeida.paula@gmail.com>
2020-07-22 21:06:22 +0100

    reset client and server done

--- neon/server/controllers/auth-controller.js
+++ neon/server/controllers/auth-controller.js
@@ -251,3 +251,56 @@ exports.forgetController = (req, res) => {
 
 }
 
+exports.resetController = (req, res) => {
+  const { resetPasswordLink, newPassword } = req.body;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      error: firstError
+    });
+  } else {
+
+    if(resetPasswordLink) {
+      jwt.verify(resetPasswordLink, process.env.JWT_RESET_PASSWORD, 
+        (err, decoded) => {
+          if(err) {
+            return res.status(400).json({
+              error: 'Expired Link, try again'
+            });
+          }
+          User.findOne({
+            resetPasswordLink
+          }).exec((err, user) => {
+            if (err || !user) {
+              return res.status(400).json({
+                error: 'Something went wrong. Try later.'
+              });
+            }
+
+            const updateFields = {
+              password: newPassword,
+              resetPasswordLink: ''
+            };
+
+            user = _.extend(user, updateFields);
+
+            user.save((err, result) => {
+              if(err) {
+                return res.status(400).json({
+                  error: 'Error reseting user password'
+                });
+              }
+
+              res.json({
+                message: 'Great! Now you can login with your new password!'
+              });
+            });
+          });
+        });
+    }
+  }
+
+}
+
--- neon/server/routes/auth-route.js
+++ neon/server/routes/auth-route.js
@@ -15,7 +15,8 @@ const {
   registerController,
   activationController,
   loginController,
-  forgetController 
+  forgetController,
+  resetController 
 } = require('../controllers/auth-controller.js');
 
 const { validationResult } = require('express-validator');
@@ -24,5 +25,6 @@ router.post('/register', validRegister, registerController);
 router.post('/login', validLogin, loginController);
 router.post('/activation', activationController);
 router.put('/password/forget', forgotPasswordValidator, forgetController);
+router.put('/password/reset', resetPasswordValidator, resetController);
 
 module.exports = router;
